9d79c0b6ffb564089541c1209def79e3229225f4
Arturo Latorre <arturola@ymail.com>
2023-03-18 15:56:36 +0100

    First Commit dawio logo & favicon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderVerticalComponent } from './header-vertical/header-vertical.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderVerticalComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/header-vertical/header-vertical.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HeaderVerticalComponent } from './header-vertical.component';
+
+describe('HeaderVerticalComponent', () => {
+  let component: HeaderVerticalComponent;
+  let fixture: ComponentFixture<HeaderVerticalComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ HeaderVerticalComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(HeaderVerticalComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
